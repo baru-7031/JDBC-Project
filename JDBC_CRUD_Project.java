5ebc1b4d0115fc936271f0563431a2e19ae47765
Subham kr Gupta <subham7031979@gmail.com>
2022-03-22 17:18:03 +0530

    Project Starting point

---
+++ JDBC_CRUD_Project.java
@@ -0,0 +1,21 @@
+//JDBC CRUD Operation Project
+
+import java.mysql.*;
+import java.util.Scanner;
+
+class JDBC_CRUD_Project
+{
+	public static void main(String[] args) 
+	{
+		
+		try{	
+
+			
+			
+
+		}catch(Exception e){
+			System.out.println(e);
+		}
+
+	}
+}
